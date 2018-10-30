08b3dda110e3dd59af9c3ca4d57748ce2c27405d
marelywoody <ros_ely_arc@hotmail.com>
2018-10-29 20:48:44 -0600

    modificando errores

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Home</router-link>
       <router-link to="/login-driver">Eres conductor? Ingresa aquí</router-link>
+      <router-view></router-view>
     </div>
+  </div>
 </template>
 
 <script>
