28b525f1fd075d130f23037b9927e519fcceeb2e
[Copelia] <iamcorallium@gmail.com>
2018-10-29 19:20:17 -0600

    Añadiendo componente Map y Login para conductor, ambos componentes enrutados

---
+++ src/components/Map.vue
@@ -0,0 +1,51 @@
+/* eslint-disable */
+<template>
+    <div class="here-map">
+        <div ref="map" v-bind:style="{ width: width, height: height }"></div>
+    </div>
+</template>
+
+<script>
+var platform = new H.service.Platform({
+  'app_id': 'LbRwyMzRp1cGXIsI0GNE',
+  'app_code': '_Zz0xbDQRqeT03IH5Q22iQ'
+})
+
+export default {
+  name: 'Map',
+  data() {
+    return {
+      map: {},
+      platform: {},
+    };
+  },
+  props: {
+    appId: String,
+    appCode: String,
+    lat: String,
+    lng: String,
+    width: String,
+    height: String,
+  },
+  created() {
+    this.platform = new H.service.Platform({
+      app_id: this.appId,
+      app_code: this.appCode,
+    });
+  },
+  mounted() {
+    this.map = new H.Map(
+      this.$refs.map,
+      this.platform.createDefaultLayers().normal.map,
+      {
+        zoom: 10,
+        center: { lng: this.lng, lat: this.lat },
+      },
+    );
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,38 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <Map
+            appId="LbRwyMzRp1cGXIsI0GNE"
+            appCode="_Zz0xbDQRqeT03IH5Q22iQ"
+            lat="37.7397"
+            lng="-121.4252"
+            width="100%"
+            height="835px" />
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+var platform = new H.service.Platform({
+  'app_id': 'LbRwyMzRp1cGXIsI0GNE',
+  'app_code': '_Zz0xbDQRqeT03IH5Q22iQ'
+})
+
+import Map from '../components/Map.vue';
 
 export default {
   name: 'home',
   components: {
-    HelloWorld,
+    Map,
   },
 };
 </script>
+
+
+<style>
+#app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+    }
+</style>
